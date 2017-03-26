7d8fe3041367fbbb6c816f091e06baaceec4ed08
juanmspoleti <elpole22@gmail.com>
2017-03-25 21:00:54 -0300

    Update tp.hs

--- tp.hs
+++ tp.hs
@@ -1,8 +1,4 @@
-data Raton = CRaton {
-	edad :: Float, 
-	peso :: Float, 
-	altura :: Float
- }deriving (Show)
+data Raton = CRaton {edad :: Float, peso :: Float, altura :: Float }deriving (Show)
 
 mickeyMouse = CRaton 88 20 0.8
 jerry = CRaton 76 2 0.3
@@ -13,28 +9,26 @@ estudioAntiguedad raton = (edad raton + 5) / 85
 analisisExceso estudio raton valor = estudio raton > valor 	
 analisisRangoMedio estudio raton valorMinimo valorMaximo = not(estudio raton > valorMinimo && estudio raton < valorMaximo)
 analisisBerreta _ _ = False
-
+	
 hierbaBuena :: Raton -> Raton
 hierbaBuena (CRaton edad peso altura) = CRaton (rejuvenecerRaton edad) peso altura
 
 hierbaMala :: Raton -> Raton
 hierbaMala (CRaton edad peso altura) = CRaton (envejecerRaton edad) peso altura
 
-alcachofa :: Raton -> Float -> Raton
-alcachofa (CRaton edad peso altura) alcachofa = CRaton edad (reducirPeso (alcachofa peso)) altura
+alcachofa :: Float -> Raton -> Raton
+alcachofa alcachofa (CRaton edad peso altura) = CRaton edad (peso - reducirPeso alcachofa peso) altura
 
 hierbaZort :: Raton -> Raton
 hierbaZort _ = CRaton 0 0 0
 
-rejuvenecerRaton = ( / )  2
-envejecerRaton = ( * ) 2
-reducirPeso porcentaje peso = peso - (alcachofa / peso) 
+rejuvenecerRaton =(/2)
+envejecerRaton = (*2)
+reducirPeso alcachofa peso = alcachofa / peso 
 
--- no podria ser:
--- mezclarHierbas hierba1 hierba2 raton = segundaHierba (primeraHierba raton) ?????
-mezclarHierbas primerHierba segundaHierba (CRaton edad peso altura) = segundaHierba(primerHierba(CRaton edad peso altura))
+medicamento raton hierbas = foldl tomarHierba raton hierbas
 
-tomarMedicamento listaDeHierbas raton = map raton listaDeHierbas
+tomarHierba raton hierba = hierba raton
 
 ratisalil :: Raton -> Raton
 ratisalil (CRaton edad peso altura) = hierbaMala(hierbaZort(CRaton edad peso altura))
