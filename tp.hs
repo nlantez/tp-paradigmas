c644d77cb9cbfc0561bc400543ab072bcb2b69e8
Juan Manuel Spoleti <Juan Manuel Spoleti>
2017-03-26 21:28:16 -0300

    asdasd

--- tp.hs
+++ tp.hs
@@ -1,4 +1,29 @@
-data Raton = CRaton {edad :: Float, peso :: Float, altura :: Float }deriving (Show)
+import Test.Hspec
+
+runTests = hspec $do
+	describe "Tests de diagnosticos:" $do
+		it "el analisis de exceso para el estudio de antiguedad con indice maximo 1 de Micky Mouse debería dar positivo" $do
+			analisisExceso estudioAntiguedad 1 mickeyMouse `shouldBe` True 
+		it "el analisis de exceso para el estudio de antiguedad con indice maximo 1 de Jerry debería dar negativo" $do
+			analisisExceso estudioAntiguedad 1 jerry `shouldBe` False 
+		it "el analisis de rango medio para el estudio de masa coporal con indices entre 18.5 y 25 de Micky Mouse deberia dar positivo" $do
+			analisisRangoMedio estudioMasaCorporal mickeyMouse 18.5 25 `shouldBe` True
+		it "el analisis de rango medio para el estudio de masa coporal con indices entre 18.5 y 25 de Jerry deberia dar negativo" $do
+			analisisRangoMedio estudioMasaCorporal jerry 18.5 25 `shouldBe` False
+	describe "Tests medicinas y operaciones:" $do
+		--it "mezclar una Hierba buena con una Hierba mala crea una hierba que no produce efecto"
+		--it ""
+		it  "el medicamento ratisalil combina una Hierba hierbaMala y una Hierba Zort que produce los mismos resultados que una Hierba Zort" $do
+			medicamento jerry [hierbaZort,hierbaMala] `shouldBe` medicamento jerry [hierbaZort]
+		it "el medicamento ponds anti age combina una alcachofa de 10 con 3 hierbas buenas que se le aplica a jerry quedando con edad = 9.5, peso = 1.8 y altura = 0.3" $do
+			medicamento jerry [alcachofa 10, hierbaBuena,hierbaBuena,hierbaBuena] `shouldBe` CRaton 9.5 1.8 0.3
+
+
+
+
+
+
+data Raton = CRaton {edad :: Float, peso :: Float, altura :: Float }deriving (Show,Eq)
 
 mickeyMouse = CRaton 88 20 0.8
 jerry = CRaton 76 2 0.3
@@ -11,8 +36,8 @@ estudioAntiguedad :: Raton -> Float
 estudioAntiguedad raton = (edad raton + 5) / 85
 
 
-analisisExceso :: (Raton -> Float) -> Raton -> Float -> Bool
-analisisExceso estudio raton valor = estudio raton > valor
+analisisExceso :: (Raton -> Float) -> Float -> Raton -> Bool
+analisisExceso estudio valor raton = estudio raton > valor
 
 analisisRangoMedio :: (Raton -> Float) -> Raton -> Float -> Float -> Bool
 analisisRangoMedio estudio raton valorMinimo valorMaximo = not(estudio raton > valorMinimo && estudio raton < valorMaximo)
@@ -24,6 +49,7 @@ analisisBerreta _ _ = False
 hierbaBuena :: Raton -> Raton
 hierbaBuena (CRaton edad peso altura) = CRaton (rejuvenecerRaton edad) peso altura
 
+
 hierbaMala :: Raton -> Raton
 hierbaMala (CRaton edad peso altura) = CRaton (envejecerRaton edad) peso altura
 
@@ -32,6 +58,7 @@ alcachofa porcentaje (CRaton edad peso altura) = CRaton edad (reducirPeso porcen
 
 hierbaZort :: Raton -> Raton
 hierbaZort _ = CRaton 0 0 0
+
 
 rejuvenecerRaton :: Float -> Float
 rejuvenecerRaton =(/2)
@@ -42,15 +69,14 @@ envejecerRaton = (*2)
 reducirPeso:: Float -> Float -> Float
 reducirPeso porcentaje peso = peso - ((porcentaje * peso)/100)
 
-
+medicamento :: Raton -> [(Raton -> Raton)] -> Raton
 medicamento raton hierbas = foldl tomarHierba raton hierbas
 
 tomarHierba :: Raton -> (Raton -> Raton) -> Raton
 tomarHierba raton hierba = hierba raton
 
+tratamiento diagnostico raton hierbas = foldl (aplicarHastaQueDeFalse diagnostico) raton hierbas 
 
-ratisalil :: Raton -> Raton
-ratisalil raton = medicamento raton [hierbaZort,hierbaMala]
-
-pondsAntiAge :: Raton -> Raton
-pondsAntiAge raton = medicamento raton [hierbaBuena,hierbaBuena,hierbaBuena,alcachofa 10]
+aplicarHastaQueDeFalse diagnostico raton hierba  
+	| diagnostico raton = hierba raton
+	| otherwise = raton
